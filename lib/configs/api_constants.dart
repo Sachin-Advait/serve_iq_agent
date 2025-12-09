9e53a8c061d0be695f80908077bd361bfa8142e1
Sachin Advait <sachinadvaita@gmail.com>
2025-12-09 14:47:17 +0530

    Fixed endpoint.

--- lib/configs/api_constants.dart
+++ lib/configs/api_constants.dart
@@ -3,6 +3,22 @@ class ApiConstants {
   static const String baseUrl = "https://msspf.duckdns.org/serveiq/api/";
   static const String wsUrl = "wss://msspf.duckdns.org/serveiq/ws";
 
+  // ---------- AUTH ----------
   static const String login = 'auth/login';
   static const String register = 'auth/register';
+
+  // ---------- AGENT ----------
+  static const String queue = 'agent/counter/queue/';
+  static const String callNext = 'agent/counter/call-next/';
+  static const String activeToken = 'agent/counter/active-token/';
+  static const String startServing = 'agent/token/start-serving/';
+  static const String completeService = 'agent/token/complete/';
+  static const String recentServices = 'agent/recent-services';
+  static const String recall = 'agent/recall';
+  static const String transfer = 'agent/transfer';
+
+  // ---------- COUNTERS ----------
+  static const String counters = 'counters';
+  static const String singleCounter = 'counters/';
+
 }
--- lib/modules/service_agent/repository/agent_repo.dart
+++ lib/modules/service_agent/repository/agent_repo.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:servelq_agent/configs/api_constants.dart';
 import 'package:servelq_agent/models/counter_model.dart';
 import 'package:servelq_agent/models/service_history.dart';
 import 'package:servelq_agent/models/token_model.dart';
@@ -13,7 +13,7 @@ class AgentRepository {
   Future<List<TokenModel>> getQueue() async {
     try {
       final response = await _apiClient.getApi(
-        'agent/counter/queue/${SessionManager.getCounter()}',
+        ApiConstants.queue + SessionManager.getCounter(),
       );
 
       if (response != null && response.statusCode == 200) {
@@ -31,25 +31,22 @@ class AgentRepository {
   Future<TokenModel> callNext() async {
     try {
       final response = await _apiClient.postApi(
-        'agent/counter/call-next/${SessionManager.getCounter()}',
+        ApiConstants.callNext + SessionManager.getCounter(),
       );
 
       if (response != null && response.statusCode == 200) {
         final data = response.data;
-        TokenModel? token;
 
         if (data is Map<String, dynamic>) {
-          token = TokenModel.fromJson(data);
+          final token = TokenModel.fromJson(data);
 
-          // Call startServing after 4 seconds
           Future.delayed(const Duration(seconds: 5), () {
-            startServing(token!.id);
+            startServing(token.id);
           });
 
           return token;
         }
       }
-
       throw Exception('Failed to call next token');
     } catch (e) {
       rethrow;
@@ -57,18 +54,20 @@ class AgentRepository {
   }
 
   Future<void> startServing(String tokenId) async {
-    await _apiClient.postApi('agent/token/start-serving/$tokenId');
+    await _apiClient.postApi(ApiConstants.startServing + tokenId);
   }
 
   Future<void> completeService(String tokenId) async {
-    final response = await _apiClient.postApi('agent/token/complete/$tokenId');
+    final response = await _apiClient.postApi(
+      ApiConstants.completeService + tokenId,
+    );
 
     if (response != null && response.statusCode != 200) {}
   }
 
   Future<List<ServiceHistory>> getRecentServices() async {
     try {
-      final response = await _apiClient.getApi('agent/recent-services');
+      final response = await _apiClient.getApi(ApiConstants.recentServices);
 
       if (response != null && response.statusCode == 200) {
         final data = response.data;
@@ -82,9 +81,8 @@ class AgentRepository {
     }
   }
 
-  // Fetch a single counter by session counter ID
   Future<CounterModel> getCounter() async {
-    final path = "counters/${SessionManager.getCounter()}";
+    final path = ApiConstants.singleCounter + SessionManager.getCounter();
     try {
       final response = await _apiClient.getApi(path);
 
@@ -103,9 +101,8 @@ class AgentRepository {
     }
   }
 
-  // Fetch all counters
   Future<List<CounterModel>> getAllCounters() async {
-    const path = "counters";
+    const path = ApiConstants.counters;
     try {
       final response = await _apiClient.getApi(path);
 
@@ -124,11 +121,10 @@ class AgentRepository {
     }
   }
 
-  // Recall a token by token ID
   Future<TokenModel> recallToken(String tokenId) async {
     try {
       final response = await _apiClient.postApi(
-        'agent/recall',
+        ApiConstants.recall,
         queryPara: {"tokenId": tokenId, "counterId": ""},
       );
 
@@ -137,7 +133,6 @@ class AgentRepository {
         if (data is Map<String, dynamic>) {
           final token = TokenModel.fromJson(data);
 
-          // Optional: automatically start serving again after recall
           Future.delayed(const Duration(seconds: 5), () {
             startServing(token.id);
           });
@@ -154,60 +149,30 @@ class AgentRepository {
     }
   }
 
-  // Add this method to AgentRepository class
-  Future<void> submitFeedback({
-    required String tokenId,
-    required int rating,
-    required String review,
-    required String counterCode,
-  }) async {
-    try {
-      final response = await _apiClient.postApi(
-        'feedback',
-        body: {
-          'tokenId': tokenId,
-          'rating': rating,
-          'review': review,
-          "counterCode": counterCode,
-          "agentName": SessionManager.getUsername(),
-        },
-      );
-
-      if (response != null && response.statusCode != 200) {
-        throw Exception('Failed to submit feedback');
-      }
-    } catch (e) {
-      rethrow;
-    }
-  }
-
   Future<void> transferService(String tokenId, String counterId) async {
     final response = await _apiClient.postApi(
-      'tokens/transfer',
+      ApiConstants.transfer,
       body: {"tokenId": tokenId, "toCounterId": counterId},
     );
 
     if (response != null && response.statusCode != 200) {
-      debugPrint("Failed to complete service");
       throw Exception('Failed to complete service');
     }
   }
 
   Future<TokenModel?> counterActiveToken() async {
     final response = await _apiClient.getApi(
-      'agent/counter/active-token/${SessionManager.getCounter()}',
+      ApiConstants.activeToken + SessionManager.getCounter(),
     );
 
     if (response != null && response.statusCode == 200) {
       final data = response.data;
-      TokenModel? token;
 
       if (data is Map<String, dynamic>) {
-        token = TokenModel.fromJson(data);
+        final token = TokenModel.fromJson(data);
 
-        // Call startServing after 4 seconds
         Future.delayed(const Duration(seconds: 5), () {
-          startServing(token!.id);
+          startServing(token.id);
         });
 
         return token;
