c1cf4854f6063ecd4aba62b2912b35e451f20a6f
Sachin Advait <sachinadvaita@gmail.com>
2025-12-29 16:37:59 +0530

    Increased the serve api calling time.

--- lib/modules/service_agent/repository/agent_repo.dart
+++ lib/modules/service_agent/repository/agent_repo.dart
@@ -40,7 +40,7 @@ class AgentRepository {
         if (data is Map<String, dynamic>) {
           final token = TokenModel.fromJson(data);
 
-          Future.delayed(const Duration(seconds: 10), () {
+          Future.delayed(const Duration(seconds: 15), () {
             startServing(token.id);
           });
 
@@ -135,7 +135,7 @@ class AgentRepository {
         if (data is Map<String, dynamic>) {
           final token = TokenModel.fromJson(data);
 
-          Future.delayed(const Duration(seconds: 10), () {
+          Future.delayed(const Duration(seconds: 15), () {
             startServing(token.id);
           });
 
